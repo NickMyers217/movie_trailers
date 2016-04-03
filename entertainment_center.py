8a3a250790e7611d6af2a42c4cd037fb788e9bbd
Nickmyers217 <nickbmyers217@gmail.com>
2016-04-03 16:32:53 -0700

    Replaced tabs with 4 spaces.

--- entertainment_center.py
+++ entertainment_center.py
@@ -7,25 +7,25 @@ movies = []
 # Create an instance of Movie to hold the data for RoboCop
 # and then append it to the list
 movies.append(media.Movie(
-	'RoboCop',
-	'http://ia.media-imdb.com/images/M/MV5BMTk1MDUzMTQ3OV5BMl5BanBnXkFtZTcwMDAwNTk0NA@@._V1_SX640_SY720_.jpg',
-	'https://www.youtube.com/watch?v=zbCbwP6ibR4'
+    'RoboCop',
+    'http://ia.media-imdb.com/images/M/MV5BMTk1MDUzMTQ3OV5BMl5BanBnXkFtZTcwMDAwNTk0NA@@._V1_SX640_SY720_.jpg',
+    'https://www.youtube.com/watch?v=zbCbwP6ibR4'
 ))
 
 # Create an instance of Movie to hold the data for First Blood
 # and then append it to the list
 movies.append(media.Movie(
-	'First Blood',
-	'http://ia.media-imdb.com/images/M/MV5BNzk5NDc4MDQyNV5BMl5BanBnXkFtZTgwNzg5NTYxMTE@._V1_SX640_SY720_.jpg',
-	'https://www.youtube.com/watch?v=IAqLKlxY3Eo'
+    'First Blood',
+    'http://ia.media-imdb.com/images/M/MV5BNzk5NDc4MDQyNV5BMl5BanBnXkFtZTgwNzg5NTYxMTE@._V1_SX640_SY720_.jpg',
+    'https://www.youtube.com/watch?v=IAqLKlxY3Eo'
 ))
 
 # Create an instance of Movie to hold the data for First Blood
 # and then append it to the list
 movies.append(media.Movie(
-	'Darkman',
-	'http://ia.media-imdb.com/images/M/MV5BMTc5MzUxMjk4NF5BMl5BanBnXkFtZTgwNTEzNDk4NjE@._V1_UY1200_CR92,0,630,1200_AL_.jpg',
-	'https://www.youtube.com/watch?v=L58rdhCfDIU'
+    'Darkman',
+    'http://ia.media-imdb.com/images/M/MV5BMTc5MzUxMjk4NF5BMl5BanBnXkFtZTgwNTEzNDk4NjE@._V1_UY1200_CR92,0,630,1200_AL_.jpg',
+    'https://www.youtube.com/watch?v=L58rdhCfDIU'
 ))
 
 # Server a formatted HTML page with the movie data
--- media.py
+++ media.py
@@ -1,18 +1,18 @@
 class Movie:
-	"""This is a class that stores data on movies
-    
+    """This is a class that stores data on movies
+
     Attributes:
         title               (str): The title of the movie
         poster_image_url    (str): The url for the movies box / poster art
-		trailer_youtube_url (str): The youtube url for the movie's trailer
+        trailer_youtube_url (str): The youtube url for the movie's trailer
     """
-	# Attributes
-	title               = ''
-	poster_image_url    = ''
-	trailer_youtube_url = ''
+    # Attributes
+    title               = ''
+    poster_image_url    = ''
+    trailer_youtube_url = ''
 
-	# Constructor
-	def __init__(self, title, poster, trailer):
-		self.title               = title
-		self.poster_image_url    = poster
-		self.trailer_youtube_url = trailer
+    # Constructor
+    def __init__(self, title, poster, trailer):
+        self.title               = title
+        self.poster_image_url    = poster
+        self.trailer_youtube_url = trailer
