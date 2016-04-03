45708f1288b8f9fd3fd4de67294a4316b3e1ae68
Nickmyers217 <nickbmyers217@gmail.com>
2016-04-03 16:29:25 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Movie Trailers
+This is a site to show off some of my favorite action movies.
+
+## Installation
+To get the site up and running, use the following commands from your shell (you'll need git and python installed).
+
+``` shell
+git clone https://github.com/nickmyers217/movie_trailers
+cd movie_trailers
+python entertainment_center.py
+```
+
+Enjoy!
---
+++ entertainment_center.py
@@ -0,0 +1,32 @@
+import media
+import fresh_tomatoes as ft
+
+# An empty list of movies
+movies = []
+
+# Create an instance of Movie to hold the data for RoboCop
+# and then append it to the list
+movies.append(media.Movie(
+	'RoboCop',
+	'http://ia.media-imdb.com/images/M/MV5BMTk1MDUzMTQ3OV5BMl5BanBnXkFtZTcwMDAwNTk0NA@@._V1_SX640_SY720_.jpg',
+	'https://www.youtube.com/watch?v=zbCbwP6ibR4'
+))
+
+# Create an instance of Movie to hold the data for First Blood
+# and then append it to the list
+movies.append(media.Movie(
+	'First Blood',
+	'http://ia.media-imdb.com/images/M/MV5BNzk5NDc4MDQyNV5BMl5BanBnXkFtZTgwNzg5NTYxMTE@._V1_SX640_SY720_.jpg',
+	'https://www.youtube.com/watch?v=IAqLKlxY3Eo'
+))
+
+# Create an instance of Movie to hold the data for First Blood
+# and then append it to the list
+movies.append(media.Movie(
+	'Darkman',
+	'http://ia.media-imdb.com/images/M/MV5BMTc5MzUxMjk4NF5BMl5BanBnXkFtZTgwNTEzNDk4NjE@._V1_UY1200_CR92,0,630,1200_AL_.jpg',
+	'https://www.youtube.com/watch?v=L58rdhCfDIU'
+))
+
+# Server a formatted HTML page with the movie data
+ft.open_movies_page(movies)
