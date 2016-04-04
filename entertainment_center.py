4478910e635827e474aaca8f63d2d1f452340903
Nickmyers217 <nickbmyers217@gmail.com>
2016-04-03 18:24:17 -0700

    Fixed some of the documentation.

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,22 +6,19 @@ movies = []
 
 # Create an instance of Movie to hold the data for RoboCop
 # and then append it to the list
-movies.append(media.Movie(
-    'tt0093870',
+movies.append(media.Movie('tt0093870',
     'https://www.youtube.com/watch?v=zbCbwP6ibR4',
     'Officer Murphy / RoboCop'))
 
 # Create an instance of Movie to hold the data for First Blood
 # and then append it to the list
-movies.append(media.Movie(
-    'tt0083944',
+movies.append(media.Movie('tt0083944',
     'https://www.youtube.com/watch?v=IAqLKlxY3Eo',
     'Jonh Rambo'))
 
-# Create an instance of Movie to hold the data for First Blood
+# Create an instance of Movie to hold the data for Darkman
 # and then append it to the list
-movies.append(media.Movie(
-    'tt0099365',
+movies.append(media.Movie('tt0099365',
     'https://www.youtube.com/watch?v=L58rdhCfDIU',
     'Peyton Westlake / Darkman'))
 
